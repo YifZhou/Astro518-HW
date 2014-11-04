c8cf77f99056ff43c966e864402d8ac1de655e63
YifZhou <zhouyifan1012@gmail.com>
2014-11-04 11:27:54 -0700

    Homework5 done

---
+++ HW1/auto/Astro518_HW_01.el
@@ -0,0 +1,81 @@
+(TeX-add-style-hook "Astro518_HW_01"
+ (lambda ()
+    (LaTeX-add-environments
+     "Shaded"
+     "notebookcell")
+    (LaTeX-add-labels
+     "estimation-sample-size-needed"
+     "generating-fake-data"
+     "rmssqrtmu-changes-with-sample-size"
+     "simulating-the-detection-of-photons")
+    (TeX-add-symbols
+     '("NormalTok" 1)
+     '("ErrorTok" 1)
+     '("RegionMarkerTok" 1)
+     '("FunctionTok" 1)
+     '("AlertTok" 1)
+     '("OtherTok" 1)
+     '("CommentTok" 1)
+     '("StringTok" 1)
+     '("CharTok" 1)
+     '("FloatTok" 1)
+     '("BaseNTok" 1)
+     '("DecValTok" 1)
+     '("DataTypeTok" 1)
+     '("KeywordTok" 1)
+     "br"
+     "gt"
+     "lt"
+     "PY"
+     "PYZbs"
+     "PYZus"
+     "PYZob"
+     "PYZcb"
+     "PYZca"
+     "PYZam"
+     "PYZlt"
+     "PYZgt"
+     "PYZsh"
+     "PYZpc"
+     "PYZdl"
+     "PYZhy"
+     "PYZsq"
+     "PYZdq"
+     "PYZti"
+     "PYZat"
+     "PYZlb"
+     "PYZrb"
+     "smaller"
+     "notebookcellframe"
+     "notebookcellframetop"
+     "notebookcellframebottom"
+     "notebookcellframemiddle"
+     "FrameCommand"
+     "FirstFrameCommand"
+     "LastFrameCommand"
+     "MidFrameCommand")
+    (TeX-run-style-hooks
+     "framed"
+     "needspace"
+     "scrextend"
+     "tikz"
+     "booktabs"
+     "longtable"
+     "hyperref"
+     "grffile"
+     "fancyvrb"
+     "inputenc"
+     "utf8x"
+     "ucs"
+     "mathletters"
+     "amssymb"
+     "amsmath"
+     "geometry"
+     "enumerate"
+     "color"
+     "adjustbox"
+     "graphicx"
+     "latex2e"
+     "art10"
+     "article")))
+
